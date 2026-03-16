0d42de0d6865950b8beb13cf506d1753322cdf79
LeraKhubulova <lera.khubulova.06@mail.ru>
2026-03-17 01:20:37 +0300

    релиз 1 квадратные уравнения

---
+++ script.js
@@ -0,0 +1,23 @@
+function quad(a,b,c){
+    if(a==0) return " Ошибка. Недопустимое значение параметра а";
+    var D=b*b-4*a*c;
+    var x1,x2;
+    if(D>0){
+        x1=(-b-Math.sqrt(D))/(2*a);
+        x2=(-b+Math.sqrt(D))/(2*a);
+        return "D = "+ D+ ", x1 = "+x1+", x2 = "+x2;
+    }
+    else if (D==0){
+        x1=(-b)/(2*a);
+        return "D = "+ D+ ", x1 = "+x1;
+    }else if (D<0) return "Корней нет"
+}
+
+function prog(a,b,c){
+    var a=Number(document.getElementById('a').value);
+    var b=Number(document.getElementById('b').value);
+    var c=Number(document.getElementById('c').value);
+    var result=quad(a,b,c);
+    document.getElementById("result").textContent=result;
+    document.getElementById("otvet").style.display = "block";
+}
